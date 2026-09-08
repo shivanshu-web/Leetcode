2d842553b79246ebf746cfd6d7212161ae80765c
shivanshu-web <sr4858244@gmail.com>
2026-09-08 16:32:41 +0530

    Time: 1 ms (83.65%), Space: 42.3 MB (93.72%) - LeetHub

---
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int countCommas(int n) {
+        if(n<1000){
+            return 0;
+        }
+    //     String s = n +"";
+    //    int nn = (s.charAt(0) - '0') * (int)Math.pow(10, s.length() - 1);
+        return n-999;
+
+        
+    }
+}
